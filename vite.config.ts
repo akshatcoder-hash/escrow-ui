85f17e6ada614df9c0ce6144525784c6083c2b0a
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-13 03:24:43 +0530

    project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import nodePolyfills from './nodePolyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': {},
+    'global': {},
+  },
 })
