f3c52113486a21ef99cc39de9bec9c3f5ddce1fd
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-14 12:08:55 +0530

    fix buffer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
+window.global = window as any;
+if (typeof global === 'undefined') {
+  (window as any).global = window;
+}
+(window as any).process = {
+  env: { NODE_ENV: process.env.NODE_ENV },
+};
+
+console.log('main.tsx is running');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
