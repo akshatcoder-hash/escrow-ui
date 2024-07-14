201088e3efd516304f1df937d2c1a6b3eee0d0d1
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-14 12:21:05 +0530

    hotfixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'crypto-browserify';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import nodePolyfills from './nodePolyfills'
+import nodePolyfills from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['crypto'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+      'crypto': 'crypto-browserify',
     },
   },
   define: {
