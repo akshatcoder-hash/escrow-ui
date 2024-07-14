ebc39846743b41f88707100dfcb348ededc4156b
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-14 12:07:12 +0530

    define constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const PROGRAM_ID = new PublicKey(
+  "8iK7UY3ZxnCVGWvqT8AaR5KLUnC1usCoAaMSZcnNbi8s"
+);
+
+export const NETWORK = "devnet";
+export const COMMITMENT = "confirmed";
+
+export const RPC_ENDPOINT = "https://api.devnet.solana.com";
